0ece0c71bcb9095b432164d0f1eecf40c2d8a15e
LeViiAcKerMan01 <priyanshutadhiyal4@gmail.com>
2024-06-19 08:30:50 +0530

    First commit

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+  display1("Priyanshu");
+  display2("Navneet");
+  return 0;
+}
+void display1(string name){
+
+   cout<<"Hello"<<name<<endl;
+}
+
+void display2(string name){
+   cout<<"Hello"<<name<<endl;
+}
+
